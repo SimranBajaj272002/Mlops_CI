3b1973e9c9a462882157864cd3a8b1ffcf3935ad
Simran Bajaj <Simran.Bajaj@friska.ai>
2026-07-04 16:03:26 +0530

    implemented CI and updated the code to test CI

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ cube = n ** 3
 fifth_power = n ** 5
 
 # Display results
+st.write(f"we are doing the calculations for the number: {n}")
 st.write(f"The square of {n} is: {square}")
 st.write(f"The cube of {n} is: {cube}")
 st.write(f"The fifth power of {n} is: {fifth_power}")
